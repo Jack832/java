f813677eed303f80bf874ec3ab06dacd740bb31d
jack832 <saurabhmore832@gmail.com>
2015-12-01 19:06:05 +0530

    while do while loop

---
+++ loops.java
@@ -0,0 +1,19 @@
+class loops
+{
+public static void main(String arg[])
+{
+int a=10;
+while (a<15)
+{
+System.out.println("the value of a"+a);
+a++;
+}
+/*int b=0;
+do
+{
+System.out.println("the value of b"+b);
+b++;
+}
+while(b<15);*/
+}
+}
