3a8d22416ed0194730149eca549efb99181d7209
jack832 <saurabhmore832@gmail.com>
2015-12-02 08:49:13 +0530

    array programs

---
+++ arrayfor.java
@@ -0,0 +1,18 @@
+class arrayfor
+{
+public static void main(String arg[])
+{
+int values[]={1,2,3,4,5};
+for(int va:values)
+{
+System.out.print(va);
+System.out.print(",");
+}
+String names[]={"surya","aarya","kamal"};
+for(String na:names)
+{
+System.out.print(na);
+System.out.print(",");
+}
+}
+}
