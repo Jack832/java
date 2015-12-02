c3e7674256bf61de31068a8d1263eb019dc38bf5
jack832 <saurabhmore832@gmail.com>
2015-12-02 08:51:12 +0530

    static final

---
+++ modifier.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class modifier
+{
+static boolean myflag;
+static final double weeks = 9.5;
+protected static final int width=40;
+
+public static void main (String arg[])
+{
+System.out.println("the value of myflag is"+myflag);
+System.out.println("the value of weeks"+weeks);
+System.out.println("the value of width"+width);
+}
+}
+
